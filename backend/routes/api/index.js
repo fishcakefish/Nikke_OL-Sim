fb06e62b699f4c569d020ce1cb132fc53c845823
fishcakefish <fishcakeow@gmail.com>
2024-01-19 15:52:49 -0800

    frontend and backend connected

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,8 +10,4 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter)
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
-
 module.exports = router;
